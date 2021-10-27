df64977f928c00f9a98643328e83e55f2b226099
Chis Caremen Madalina <carmen.chis@stud.ubbcluj.ro>
2021-10-27 10:43:41 +0300

    Laborator5-pb1-1.1

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from UserInterface.console import run_ui
 def main():
     cheltuieli = []
     cheltuieli = adaugare(cheltuieli, 1, 1, 11.1, '11.07.2002', 'intretinere')
+    cheltuieli = adaugare(cheltuieli, 2, 3, 345, '29.08.2020', 'canal')
+    cheltuieli = adaugare(cheltuieli, 3, 5, 635.75, '03.06.2021', 'alte cheltuieli')
     cheltuieli = run_ui(cheltuieli)
 
 
